eceda0aa820f460131e998c687849b59b72b689c
Tichakorn <tichakorn06@hotmail.com>
2014-08-27 09:56:20 +0700

    lascatalinas 27.08.2014

--- lascatalinas/lascatalinas.js
+++ lascatalinas/lascatalinas.js
@@ -1,7 +1,25 @@
+function responsive_carousel(){
+    //set new height for responsive carousel
+    $('#fullscreen-slider .carousel').each(function( index ) {
+        my_img=$(this).find('.carousel-image img:visible').last();
+        height = my_img.height();
+        
+        $(this).height(my_img.height());
+    });
+}
+
 $(document).ready(function () {
     $("#fullscreen-slider").insertBefore("#fullbgnav");
     $(".portlet-gallaria").replaceWith($("#listing-images"));
     if($('.carousel').length>0){
-         $('#portal-logo').css("position","absolute");
+        $('#portal-logo').css("position","absolute");
     }
+    if($('#fullscreen-slider .carousel').length>0){
+        responsive_carousel();
+    }
+    $(window).resize(function() {
+        if($('#fullscreen-slider .carousel').length>0){
+            responsive_carousel();
+        } 
+    });
 });
